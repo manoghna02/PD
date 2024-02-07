89bd45b3de1d2dd35dd49adacdcdcadd8fe39013
manoghna02 <112636468+manoghna02@users.noreply.github.com>
2024-02-07 12:58:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,11 @@ import matplotlib.pyplot as plt
 # col1, col2 = st.columns([1, 3])
 
 st.title('Phishing Website Detection using Machine Learning')
-st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
-
 
 with st.expander("PROJECT DETAILS"):
     st.subheader('Approach')
-    st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
-             'I benefit from content-based approach and focus on html of the websites. '
-             'Also, I used scikit-learn for the ML models.'
+    st.write('We used _supervised learning_ to classify phishing and legitimate websites. '
              )
-    st.write('For this educational project, '
-             'I created my own data set and defined features, some from the literature and some based on manual analysis. '
-             'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
-    st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
-
-    st.subheader('Data set')
-    st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
-    st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-    st.write('Data set was created in October 2022.')
 
     # ----- FOR THE PIE CHART ----- #
     labels = 'phishing', 'legitimate'
@@ -62,11 +47,11 @@ with st.expander("PROJECT DETAILS"):
     )
 
     st.subheader('Features')
-    st.write('I used only content-based features. I didn\'t use url-based faetures like length of url etc.'
+    st.write('We used only content-based features. We didn\'t use url-based faetures like length of url etc.'
              'Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
 
     st.subheader('Results')
-    st.write('I used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
+    st.write('We used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
              'Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.'
              'Comparison table is below:')
     st.table(ml.df_results)
@@ -79,10 +64,8 @@ with st.expander("PROJECT DETAILS"):
     st.write('KN --> K-Neighbours')
 
 with st.expander('EXAMPLE PHISHING URLs:'):
-    st.write('_https://rtyu38.godaddysites.com/_')
-    st.write('_https://karafuru.invite-mint.com/_')
-    st.write('_https://defi-ned.top/h5/#/_')
-    st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
+    st.write('_https://!/hosts//_')
+    st.caption('PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
 
 choice = st.selectbox("Please select your machine learning model",
                  [
