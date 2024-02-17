a790b826f35a4188956128a543c092c8584266ea
manoghna02 <112636468+manoghna02@users.noreply.github.com>
2024-02-17 11:52:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with st.expander("PROJECT DETAILS"):
     st.write('KN --> K-Neighbours')
 
 with st.expander('EXAMPLE PHISHING URLs:'):
-    st.write('_https://!/hosts//_')
+    st.write('_https://appdecentralized.pages.dev/app/_')
     st.caption('PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
 
 choice = st.selectbox("Please select your machine learning model",
